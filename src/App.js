f9a168379901eea070cc9f44dcb7bac692bc4af3
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2024-05-02 11:51:03 +0800

    -- tried design system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import DesignSystem from "./pages/DesignSystem.js";
+import Home from "./pages/Home.js";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/design-system" element={<DesignSystem />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
